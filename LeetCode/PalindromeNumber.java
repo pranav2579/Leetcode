461de3573eb845ba6e4a74476437012ea972c302
Pranav Tripathi <prtripat@microsoft.com>
2020-04-02 00:07:23 +0530

    leetcode_palindrome

---
+++ LeetCode/PalindromeNumber.java
@@ -0,0 +1,28 @@
+package CodingPractice.LeetCode;
+
+public class PalindromeNumber
+{
+    public boolean isPalindrome(int x) {
+        
+        if(x<0)
+        return false;
+        int original_num = x;
+        long reverse = 0;
+        int remainder = 0;
+        while (x>0){
+            remainder = x%10;
+            reverse = reverse*10 + remainder;
+            x = x/10;
+        }
+
+        if( (int)reverse ==  original_num){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
