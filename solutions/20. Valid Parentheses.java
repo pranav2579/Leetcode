6a319b1f0136282b0d3d39dd8b702e6b21cccedb
Pranav Tripathi <pranav2579@gmail.com>
2021-01-14 21:36:11 +0530

    1 ms

---
+++ solutions/20. Valid Parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> stack = new Stack<Character>();
+        char[] character = s.toCharArray();
+        
+    for (char c : character) {
+​
+        if(c=='{' || c == '[' || c == '(') {
+            stack.push(c);
+        }
+        else if(c == '}' && (stack.size()== 0 || stack.pop() != '{' )){
+            return false;
+        }
+​
+        else if(c == ')' && (stack.size()== 0 || stack.pop() != '(' )){
+            return false;
+        }
+​
+        else if(c == ']' && (stack.size()== 0 || stack.pop() != '[' )){
+            return false;
+        }
+    }
+​
+    // When the string is over stack should be empty
+    if(stack.size()!=0)
+        return false;
+​
+    return true;
+​
+    }
+}
