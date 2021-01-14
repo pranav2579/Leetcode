4580ef5431176d9ca8be780848db734f0341593c
Pranav Tripathi <pranav2579@gmail.com>
2021-01-14 21:38:03 +0530

    0 ms

---
+++ solutions/268. Missing Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        for(int i = 1;i<=nums.length ; i++)
+        {
+            xor = xor ^ i;
+        }
+        for(int i = 0;i<nums.length ; i++)
+        {
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    }
+}
