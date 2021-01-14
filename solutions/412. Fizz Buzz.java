6c6e0b0fb60d5cda9d153421b507fa70f47ba1c4
Pranav Tripathi <pranav2579@gmail.com>
2021-01-14 21:38:23 +0530

    1 ms

---
+++ solutions/412. Fizz Buzz.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> output = new ArrayList<String>(n);
+        for (int i = 1; i <= n; i++) {
+            if(i%15 == 0){
+                output.add("FizzBuzz");
+            } 
+            else if(i%5 == 0){
+                output.add("Buzz");
+            } 
+            else if(i%3 == 0){
+                output.add("Fizz");
+            }
+            else {
+                output.add(Integer.toString(i));
+            } 
+        }
+        return output;
+    }
+}
